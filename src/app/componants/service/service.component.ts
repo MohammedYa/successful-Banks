f2e93ace7b1b9e1649935f321755dc5e7f90a709
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-01-01 18:31:30 +0200

    edit

--- src/app/componants/service/service.component.ts
+++ src/app/componants/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './service.component.sass'
 })
 export class ServiceComponent {
+ constructor(private _Router:Router){
 
+ }
+ raute(){
+ this._Router.navigateByUrl('/queation')
+ }
 }
